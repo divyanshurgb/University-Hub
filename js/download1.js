db285ac66d2a2c12ff5f3bdbb418d9325921220c
divyanshurgb <divic309@gmail.com>
2020-09-28 22:11:07 +0530

    New Changes

---
+++ js/download1.js
@@ -0,0 +1,194 @@
+ var stateObject = {
+        "1": {
+            
+            "Mechanical Engineering":[ "Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Computer Science and Engineering/IT":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Information Technology":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Electronics and Communication Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Agricultural Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Textile Chemistry":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Textile Technology":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Electrical Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Electrical and Electronics Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Food Technology":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Civil Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Environmental Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Carpet & Textile Technology":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Aeronautical Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Plastics Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Production Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Bio Technology":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"],
+            "Chemical Engineering":["Physic" , "Chemistry", "Basic Mathematics-I","Basic Electrical Engineering","Programming for Problem Solving","Engineering Graphics and Design","Workshop ","Mathematics-II"]
+            
+        },
+         "2":{
+            "Mechanical Engineering":[],
+            "Computer Science and Engineering/IT":[],
+            "Information Technology":[],
+            "Electronics and Communication Engineering":[],
+            "Agricultural Engineering":[],
+            "Textile Chemistry":[],
+            "Textile Technology":[],
+            "Electrical Engineering":[],
+            "Electrical and Electronics Engineering":[],
+            "Food Technology":[],
+            "Civil Engineering":[],
+            "Environmental Engineering":[],
+            "Carpet & Textile Technology":[],
+            "Aeronautical Engineering":[],
+            "Plastics Engineering":[],
+            "Production Engineering":[],
+            "Bio Technology":[],
+            "Chemical Engineering":[]
+
+        },
+        "3":{
+            "Mechanical Engineering":[],
+            "Computer Science and Engineering/IT":[],
+            "Information Technology":[],
+            "Electronics and Communication Engineering":[],
+            "Agricultural Engineering":[],
+            "Textile Chemistry":[],
+            "Textile Technology":[],
+            "Electrical Engineering":[],
+            "Electrical and Electronics Engineering":[],
+            "Food Technology":[],
+            "Civil Engineering":[],
+            "Environmental Engineering":[],
+            "Carpet & Textile Technology":[],
+            "Aeronautical Engineering":[],
+            "Plastics Engineering":[],
+            "Production Engineering":[],
+            "Bio Technology":[],
+            "Chemical Engineering":[]
+        },
+        "4":{
+            "Mechanical Engineering":[],
+            "Computer Science and Engineering/IT":[],
+            "Information Technology":[],
+            "Electronics and Communication Engineering":[],
+            "Agricultural Engineering":[],
+            "Textile Chemistry":[],
+            "Textile Technology":[],
+            "Electrical Engineering":[],
+            "Electrical and Electronics Engineering":[],
+            "Food Technology":[],
+            "Civil Engineering":[],
+            "Environmental Engineering":[],
+            "Carpet & Textile Technology":[],
+            "Aeronautical Engineering":[],
+            "Plastics Engineering":[],
+            "Production Engineering":[],
+            "Bio Technology":[],
+            "Chemical Engineering":[]
+        },
+        "5":{
+            "Mechanical Engineering":[],
+            "Computer Science and EngineeringIT":[],
+            "Information Technology":[],
+            "Electronics and Communication Engineering":[],
+            "Agricultural Engineering":[],
+            "Textile Chemistry":[],
+            "Textile Technology":[],
+            "Electrical Engineering":[],
+            "Electrical and Electronics Engineering":[],
+            "Food Technology":[],
+            "Civil Engineering":[],
+            "Environmental Engineering":[],
+            "Carpet & Textile Technology":[],
+            "Aeronautical Engineering":[],
+            "Plastics Engineering":[],
+            "Production Engineering":[],
+            "Bio Technology":[],
+            "Chemical Engineering":[]
+        },
+        "6":{
+            "Mechanical Engineering":[],
+            "Computer Science and Engineering/IT":[],
+            "Information Technology":[],
+            "Electronics and Communication Engineering":[],
+            "Agricultural Engineering":[],
+            "Textile Chemistry":[],
+            "Textile Technology":[],
+            "Electrical Engineering":[],
+            "Electrical and Electronics Engineering":[],
+            "Food Technology":[],
+            "Civil Engineering":[],
+            "Environmental Engineering":[],
+            "Carpet & Textile Technology":[],
+            "Aeronautical Engineering":[],
+            "Plastics Engineering":[],
+            "Production Engineering":[],
+            "Bio Technology":[],
+            "Chemical Engineering":[]
+        },
+        "7":{
+          "Mechanical Engineering":[],
+            "Computer Science and Engineering/IT":[],
+            "Information Technology":[],
+            "Electronics and Communication Engineering":[],
+            "Agricultural Engineering":[],
+            "Textile Chemistry":[],
+            "Textile Technology":[],
+            "Electrical Engineering":[],
+            "Electrical and Electronics Engineering":[],
+            "Food Technology":[],
+            "Civil Engineering":[],
+            "Environmental Engineering":[],
+            "Carpet & Textile Technology":[],
+            "Aeronautical Engineering":[],
+            "Plastics Engineering":[],
+            "Production Engineering":[],
+            "Bio Technology":[],
+            "Chemical Engineering":[]
+        },
+        "8":{
+          "Mechanical Engineering":[],
+            "Computer Science and Engineering/IT":[],
+            "Information Technology":[],
+            "Electronics and Communication Engineering":[],
+            "Agricultural Engineering":[],
+            "Textile Chemistry":[],
+            "Textile Technology":[],
+            "Electrical Engineering":[],
+            "Electrical and Electronics Engineering":[],
+            "Food Technology":[],
+            "Civil Engineering":[],
+            "Environmental Engineering":[],
+            "Carpet & Textile Technology":[],
+            "Aeronautical Engineering":[],
+            "Plastics Engineering":[],
+            "Production Engineering":[],
+            "Bio Technology":[],
+            "Chemical Engineering":[]
+        },
+
+       
+
+
+    }
+    window.onload = function () {
+        var stateSel = document.getElementById("stateSel"),
+            countySel = document.getElementById("countySel"),
+            citySel = document.getElementById("citySel");
+        for (var state in stateObject) {
+            stateSel.options[stateSel.options.length] = new Option(state, state);
+        }
+        stateSel.onchange = function () {
+            countySel.length = 1; // remove all options bar first
+            citySel.length = 1; // remove all options bar first
+            if (this.selectedIndex < 1) return; // done   
+            for (var county in stateObject[this.value]) {
+                countySel.options[countySel.options.length] = new Option(county, county);
+            }
+        }
+        stateSel.onchange(); // reset in case page is reloaded
+        countySel.onchange = function () {
+            citySel.length = 1; // remove all options bar first
+            if (this.selectedIndex < 1) return; // done   
+            var cities = stateObject[stateSel.value][this.value];
+            for (var i = 0; i < cities.length; i++) {
+                citySel.options[citySel.options.length] = new Option(cities[i], cities[i]);
+            }
+        }
+    }
